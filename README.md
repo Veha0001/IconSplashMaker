0a64cf0ea17db1e185e5dc378d9a61d795221904
Veha Veha <84841152+Veha0001@users.noreply.github.com>
2025-08-18 12:44:41 +0700

    Updated 2025 with AI

--- README.md
+++ README.md
@@ -1,46 +1,34 @@
-#Script to generate icons and splash for iOS/Android
-##Introduction
-Hey guys! It's a python script to generate icons and screenshots for iOS and Android Apps. 
+# IconSplashMaker
 
-Check this,you will find out what we do.
+Hey guys! It's a python script to generate icons and screenshots for iOS and Android Apps.
 
-![demo](https://github.com/winterfeel/IconSplashMaker/blob/master/demo.png "demo")
+## Environment
 
-Now we support iOS 10!
-
-##Environment
-Python 2.7
+Python 3.10
 
 PIL or Pillow
 
-##How to use
-in command line or terminal:
->python tool.py [action] [filename] [platform]
+## How to use
 
-* action：icon or screenshot
-* filename：your icon filename(screenshot not needed)
-* platform：ios or android
+To install and run the command
 
-Examples:
+```bash
+pip install pillow
+python tool.py -h
+```
 
->To generate iOS icon：python tool.py icon icon.jpg ios
+## Tips
 
->To generate Android icon：python tool.py icon icon.jpg android
+- You need to prepare a 'mask.png' to crop Android icon. It's size must be (512px,512px) and 70px cornerRadius.
 
->To generate iOS screenshots：python tool.py screenshot ios
+- Dont worry, we've already prepare one for you in GitHub.
 
->To generate Android screenshots：python tool.py screenshot android
+- When you generate screenshots,the script will scan all JPG/PNG file in current folder,and you don't need to worry about the orientation.
 
-##Tips
-* You need to prepare a 'mask.png' to crop Android icon. It's size must be (512px,512px) and 70px cornerRadius.
+### About
 
-	Dont worry,we've already prepare one for you in GitHub.
-
-* When you generate screenshots,the script will scan all JPG/PNG file in current folder,and you don't need to worry about the orientation.
-
-#About
-Hello,I'm Arsene,from China.
+Hello, I'm Arsene, from China.
 
 I'm a individual full stack developer,I love evey kinds of codes,Ahahahahah...
 
-My Blog:[www.winterfeel.com ](http://www.winterfeel.com )
+My Blog: [winterfeel](http://www.winterfeel.com)
--- tool.py
+++ tool.py
@@ -1,97 +1,156 @@
-#encoding=utf-8
-#by 不灭的小灯灯
-#create date 2016/5/22
-#update 2016/9/21
-#support iOS 10
-#site www.winterfeel.com
+#!/usr/bin/env python3
+# encoding=utf-8
+# by 不灭的小灯灯 (updated for Python3 & argparse)
+# create date 2016/5/22
+# update 2025/8/18
+# support iOS 10+
+# site www.winterfeel.com
+
 import os
-import sys
+import argparse
 from PIL import Image
 
-iosSizes = ['20@1x','20@2x','20@3x','29@1x','29@2x','29@3x','40@1x','40@2x','40@3x','60@2x','60@3x','60@3x','76@1x','76@2x','167@1x']
-androidSizes = [32,48,72,96,144,192]
-androidNames = ['ldpi','mdpi','hdpi','xhdpi','xxhdpi','xxxhdpi']
+# iOS icon sizes
+iosSizes = [
+    "20@1x",
+    "20@2x",
+    "20@3x",
+    "29@1x",
+    "29@2x",
+    "29@3x",
+    "40@1x",
+    "40@2x",
+    "40@3x",
+    "60@2x",
+    "60@3x",
+    "76@1x",
+    "76@2x",
+    "167@1x",
+]
 
-sizesiOS = [(640,960),(640, 1136),(750, 1334),(1242, 2208),(1536, 2048),(2048, 2732)]
-foldersiOS = ['iPhone4s','iPhone5','iPhone6','iPhone6plus','iPad','iPadLarge']
+# Android icon sizes
+androidSizes = [32, 48, 72, 96, 144, 192]
+androidNames = ["ldpi", "mdpi", "hdpi", "xhdpi", "xxhdpi", "xxxhdpi"]
 
-sizesAndroid = [(480,800),(720,1280),(1080,1920)]
-foldersAndroid = ['480x800','720x1280','1080x1920']
+# Screenshot sizes
+sizesiOS = [
+    (640, 960),
+    (640, 1136),
+    (750, 1334),
+    (1242, 2208),
+    (1536, 2048),
+    (2048, 2732),
+]
+foldersiOS = ["iPhone4s", "iPhone5", "iPhone6", "iPhone6plus", "iPad", "iPadLarge"]
 
-def processIcon(filename,platform):
-	icon = Image.open(filename).convert("RGBA")
-	if icon.size[0] != icon.size[1]:
-		print 'Icon file must be a rectangle!'
-		return
-	if platform == 'android':
-		#安卓圆角
-		mask = Image.open('mask.png')
-		r,g,b,a = mask.split()
-		icon.putalpha(a)
-		if not os.path.isdir('androidIcon'):
-			os.mkdir('androidIcon')
-		index = 0
-		for size in androidSizes:
-			im = icon.resize((size,size),Image.BILINEAR)
-			im.save('androidIcon/icon-'+ androidNames[index]+'.png')
-			index = index + 1
-	else:
-		if not os.path.isdir('iosIcon'):
-			os.mkdir('iosIcon')
-		for size in iosSizes:
-			originalSize = int(size.split('@')[0])#原始尺寸
-			multiply = int(size.split('@')[1][0:1])#倍数
-			im = icon.resize((originalSize*multiply,originalSize*multiply),Image.BILINEAR)
-			im.save('iosIcon/icon'+size+'.png')
-	print 'Congratulations!It\'s all done!'
+sizesAndroid = [(480, 800), (720, 1280), (1080, 1920)]
+foldersAndroid = ["480x800", "720x1280", "1080x1920"]
 
-def walk_dir(dir,platform):
-	files = os.listdir(dir)
-	for name in files:
-		if name.split('.')[-1] == 'jpg' or name.split('.')[-1] == 'png':#处理jpg和png
-			produceImage(name,platform)
-	print 'Congratulations!It\'s all done!'
 
-def produceImage(filename,platform):
-	print 'Processing:' + filename
-	img = Image.open(filename)
-	index = 0
-	sizes = sizesiOS
-	folders = foldersiOS
-	if platform == 'android':#默认ios，如果是安卓
-		sizes = sizesAndroid
-		folders = foldersAndroid
-	for size in sizes:
-		if not os.path.isdir(folders[index]):
-			os.mkdir(folders[index])
-		if img.size[0] > img.size[1]:#如果是横屏，交换坐标
-			im = img.resize((size[1],size[0]),Image.BILINEAR)
-			im.save(folders[index]+'/'+filename)
-		else:
-			im = img.resize(size,Image.BILINEAR)
-			im.save(folders[index]+'/'+filename)
-		index = index + 1
+def processIcon(filename, platform):
+    """Generate icons for iOS or Android"""
+    icon = Image.open(filename).convert("RGBA")
+    if icon.size[0] != icon.size[1]:
+        print("Error: Icon file must be a square!")
+        return
 
-action = sys.argv[1]#action:icon or screenshot
-if action == 'screenshot':	
-	platform = sys.argv[2]#platform
-	if platform == 'ios':
-		walk_dir('./','ios')
-	elif platform == 'android':
-		walk_dir('./','android')
-	else:
-		print 'Hey,Platform can only be "ios" or "android" !'
-elif action == 'icon':
-	filename = sys.argv[2]#image filename
-	platform = sys.argv[3]#platform
-	if not os.path.exists(filename):
-		print 'Hey,File Not Found!'
-	else:
-		if platform == 'ios':
-			processIcon(filename,'ios')
-		elif platform == 'android':
-			processIcon(filename,'android')
-		else:
-			print 'Hey,Platform can only be "ios" or "android" !'
-else:
-	print 'Hey,action can only be "icon" or "screenshot" !'
+    if platform == "android":
+        # Apply Android mask (if exists)
+        if os.path.exists("mask.png"):
+            mask = Image.open("mask.png")
+            _, _, _, a = mask.split()
+            icon.putalpha(a)
+
+        outdir = os.path.join("media", "android", "icons")
+        os.makedirs(outdir, exist_ok=True)
+
+        for index, size in enumerate(androidSizes):
+            im = icon.resize((size, size), Image.Resampling.BILINEAR)
+            im.save(os.path.join(outdir, f"icon-{androidNames[index]}.png"))
+
+    else:  # iOS
+        outdir = os.path.join("media", "ios", "icons")
+        os.makedirs(outdir, exist_ok=True)
+
+        for size in iosSizes:
+            originalSize = int(size.split("@")[0])
+            multiply = int(size.split("@")[1][0:1])
+            im = icon.resize(
+                (originalSize * multiply, originalSize * multiply),
+                Image.Resampling.BILINEAR,
+            )
+            im.save(os.path.join(outdir, f"icon{size}.png"))
+
+    print(f"{platform.capitalize()} icons generated at {outdir}")
+
+
+def walk_dir(directory, platform):
+    """Process all images in a folder as screenshots"""
+    files = os.listdir(directory)
+    for name in files:
+        if name.lower().endswith((".jpg", ".png")):
+            produceImage(os.path.join(directory, name), platform)
+
+    print(
+        f"{platform.capitalize()} screenshots generated under media/{platform}/screenshots/"
+    )
+
+
+def produceImage(filename, platform):
+    """Resize screenshots for iOS or Android"""
+    print(f"Processing: {filename}")
+    img = Image.open(filename)
+
+    if platform == "android":
+        sizes, folders = sizesAndroid, foldersAndroid
+    else:
+        sizes, folders = sizesiOS, foldersiOS
+
+    for index, size in enumerate(sizes):
+        outdir = os.path.join("media", platform, "screenshots", folders[index])
+        os.makedirs(outdir, exist_ok=True)
+
+        if img.size[0] > img.size[1]:  # landscape
+            im = img.resize((size[1], size[0]), Image.Resampling.BILINEAR)
+        else:
+            im = img.resize(size, Image.Resampling.BILINEAR)
+
+        im.save(os.path.join(outdir, os.path.basename(filename)))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Generate app icons or screenshots for iOS/Android."
+    )
+    subparsers = parser.add_subparsers(dest="action", required=True)
+
+    # Icon subcommand
+    icon_parser = subparsers.add_parser("icon", help="Generate icons")
+    icon_parser.add_argument("filename", help="Source image file (must be square)")
+    icon_parser.add_argument(
+        "platform", choices=["ios", "android"], help="Target platform"
+    )
+
+    # Screenshot subcommand
+    screenshot_parser = subparsers.add_parser("screenshot", help="Generate screenshots")
+    screenshot_parser.add_argument(
+        "platform", choices=["ios", "android"], help="Target platform"
+    )
+    screenshot_parser.add_argument(
+        "--dir", default="./", help="Directory containing images (default: ./)"
+    )
+
+    args = parser.parse_args()
+
+    if args.action == "icon":
+        if not os.path.exists(args.filename):
+            print("Error: File not found!")
+            return
+        processIcon(args.filename, args.platform)
+
+    elif args.action == "screenshot":
+        walk_dir(args.dir, args.platform)
+
+
+if __name__ == "__main__":
+    main()
